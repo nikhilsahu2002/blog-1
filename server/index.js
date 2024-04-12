cc9be0af4ad8d92f1103ff1a83aa422c22d20bd2
NIkhil Sahu <117375980+nikhilsahu2002@users.noreply.github.com>
2024-04-12 09:34:20 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,27 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
-
-import Connection from './database/db.js';
-import Router from './routes/route.js';
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
 
 dotenv.config();
 
-
 const app = express();
 
-app.use('/',Router);
+app.use("/", Router);
 app.use(cors());
-app.use(bodyParser.json({extended: true}));
-app.use((bodyParser.url({extended:true})));
-
+app.use(bodyParser.json({ extended: true }));
+// app.use((bodyParser.url({extended:true})));
 
 const PORT = 7000;
 
-app.listen(PORT,()=> console.log(`Server is runing successfully on PORT ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is runing successfully on PORT ${PORT}`),
+);
 const USERNAME = process.env.DB_USERNAME;
-const PASSWORD =process.env.DB_PASSWORD;
+const PASSWORD = process.env.DB_PASSWORD;
 
-Connection(USERNAME,PASSWORD);
+Connection(USERNAME, PASSWORD);
